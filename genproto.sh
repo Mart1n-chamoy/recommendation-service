28757e09f10f4709fd41f96a7429a12885d6c5b2
Mart1n chamoy <2004colussi@gmail.com>
2025-06-24 17:06:32 -0300

    Agregado de archivos proto

--- genproto.sh
+++ genproto.sh
@@ -21,6 +21,7 @@
 # requires gRPC tools:
 #   pip install -r requirements.txt
 
-python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
+#python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/demo.proto
+python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. ./protos/demo.proto
 
 # [END gke_recommendationservice_genproto]
